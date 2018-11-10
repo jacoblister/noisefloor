f2ed9c6b5afc7ab173a85b3acf4d49cb90b06a90
Jacob Lister <jacob.lister@nzregen.co.nz>
2018-11-10 18:08:46 +1300

    Begin react keyboard implementation

--- js/frontend/component/keyboard.go
+++ js/frontend/component/keyboard.go
@@ -1,6 +1,7 @@
 package component
 
 import (
+	"github.com/gopherjs/gopherjs/js"
 	. "github.com/jacoblister/noisefloor/common"
 
 	"github.com/bep/gr"
@@ -9,42 +10,67 @@ import (
 	"github.com/bep/gr/svga"
 )
 
+// Keyboard is an onscreen MIDI keyboard
 type Keyboard struct {
 	*gr.This
+	keydown [12]bool
 }
 
-// Implements the StateInitializer interface.
-func (k Keyboard) GetInitialState() gr.State {
-	return gr.State{"keydown": false}
+var keyMap = map[string]int{
+	"a": 60, "w": 61, "s": 62, "e": 63, "d": 64,
+	"f": 65, "t": 66, "g": 67, "y": 68, "h": 69, "u": 70, "j": 71, "k": 72,
 }
 
-// Implements the Renderer interface.
-func (k Keyboard) Render() gr.Component {
-	var depressed gr.Modifier
-	if k.State().Bool("keydown") {
-		depressed = gr.CSS("depressed")
+// GetInitialState sets up the keyboard state.
+func (k *Keyboard) GetInitialState() gr.State {
+	return gr.State{"keydown": k.keydown}
+}
+
+func (k *Keyboard) renderKey(keyNumber int, depressed bool) *gr.Element {
+	var depressedElem gr.Modifier
+	if depressed {
+		depressedElem = gr.CSS("depressed")
 	}
 
-	elem := svg.Rect(
+	key := svg.Rect(
 		gr.CSS("key-white"),
-		depressed,
-		svga.X(10),
+		depressedElem,
+		svga.X(keyNumber*20),
 		svga.Y(10),
-		svga.Width(100),
+		svga.Width(20),
 		svga.Height(40),
 		evt.MouseDown(func(event *gr.Event) {
-			k.SetState(gr.State{"keydown": true})
+			k.keydown[keyNumber] = true
+			k.SetState(gr.State{"keydown": k.keydown})
 		}),
 		evt.MouseUp(func(event *gr.Event) {
-			k.SetState(gr.State{"keydown": false})
+			k.keydown[keyNumber] = false
+			k.SetState(gr.State{"keydown": k.keydown})
 		}),
 	)
+	return key
+}
+
+// Render displays the keyboard.
+func (k *Keyboard) Render() gr.Component {
+	elem := svg.G()
+	for i := 0; i < 12; i++ {
+		key := k.renderKey(i, k.keydown[i])
+		key.Modify(elem)
+	}
+
 	return elem
 }
 
-// Implements the ShouldComponentUpdate interface.
-func (k Keyboard) ShouldComponentUpdate(next gr.Cops) bool {
-	return k.State().HasChanged(next.State, "keydown")
+// ComponentDidMount registers DOM event handler for physical keyboard actions
+func (k *Keyboard) ComponentDidMount() {
+	doc := js.Global.Get("document")
+	doc.Call("addEventListener", "keydown", func(event *js.Object) {
+		midiKey := keyMap[event.Get("key").String()]
+		if midiKey != 0 {
+			println("key pressed", midiKey)
+		}
+	})
 }
 
 // GetMIDIEvents returns the currently pending MIDI events
