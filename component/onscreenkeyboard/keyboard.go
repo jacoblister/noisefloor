29a46f4022eb14bc001f1f10d82f7be997271391
Jacob Lister <jacob.lister@nzregen.co.nz>
2019-04-04 16:21:37 +1300

    Begin onscreenkeyboard vdom implementation

--- component/onscreenkeyboard/keyboard.go
+++ component/onscreenkeyboard/keyboard.go
@@ -2,12 +2,12 @@ package onscreenkeyboard
 
 import "github.com/jacoblister/noisefloor/common/midi"
 
-type KeyboardViewer interface {
-	SendEvent(event midi.EventData)
-}
+const keyMax = 127
+const velocityMax = 127
 
 // Keyboard is the onscreen keyboard processor
 type Keyboard struct {
+	keydown    [keyMax]bool
 	MidiEvents []midi.Event
 }
 
@@ -20,11 +20,7 @@ func (k *Keyboard) Stop() {
 }
 
 // Process processes a block of samples and midi events
-func (k *Keyboard) Process(samplesIn [][][]float32, samplesOut [][][]float32, midiIn []midi.Event, midiOut []midi.Event) {
-	midiOut = k.MidiEvents
+func (k *Keyboard) Process(samplesIn [][][]float32, samplesOut [][][]float32, midiIn []midi.Event, midiOut *[]midi.Event) {
+	*midiOut = k.MidiEvents
 	k.MidiEvents = nil
-}
-
-func (k *Keyboard) SendEvent(event midi.EventData) {
-	k.MidiEvents = append(k.MidiEvents, midi.MakeMidiEvent(event.Time, event.Data))
 }
---
+++ component/onscreenkeyboard/keyboard_frontend.go
@@ -0,0 +1,127 @@
+package onscreenkeyboard
+
+import (
+	"github.com/jacoblister/noisefloor/common/midi"
+	"github.com/jacoblister/noisefloor/vdom"
+)
+
+func (k *Keyboard) noteEvent(keyNumber int, keyDown bool) {
+	if k.keydown[keyNumber] == keyDown {
+		// return early if key already is same state
+		return
+	}
+
+	k.keydown[keyNumber] = keyDown
+
+	velocity := 0
+	if keyDown {
+		velocity = velocityMax
+	}
+	midiEvent := midi.NoteOnEvent{GenericEvent: midi.GenericEvent{Time: 0, Channel: 1},
+		Note: keyNumber, Velocity: velocity}
+
+	k.MidiEvents = append(k.MidiEvents, midiEvent)
+	vdom.UpdateComponent(k)
+}
+
+func (k *Keyboard) renderKey(keyNumber int, isBlack bool, xPosition int, depressed bool) vdom.Element {
+	var depressedElem vdom.Attr
+	if depressed {
+		depressedElem = vdom.Attr{Name: "style", Value: "depressed"}
+	}
+
+	keyType := "key-white"
+	width := 40
+	height := 160
+	if isBlack {
+		keyType = "key-black"
+		xPosition += 28
+		width = 26
+		height = 120
+	}
+
+	key := vdom.MakeElement("rect",
+		"class", keyType,
+		depressedElem,
+		"x", xPosition,
+		"y", 10,
+		"width", width,
+		"height", height,
+		// evt.MouseDown(func(event *gr.Event) {
+		// 	k.noteEvent(keyNumber, true)
+		// }),
+		// evt.MouseUp(func(event *gr.Event) {
+		// 	k.noteEvent(keyNumber, false)
+		// }),
+		// evt.MouseOut(func(event *gr.Event) {
+		// 	k.noteEvent(keyNumber, false)
+		// }),
+		// evt.MouseEnter(func(event *gr.Event) {
+		// 	if event.Get("buttons").Int() != 0 {
+		// 		k.noteEvent(keyNumber, true)
+		// 	}
+		// }),
+		// evt.TouchStart(func(event *gr.Event) {
+		// 	k.noteEvent(keyNumber, true)
+		// }),
+		// evt.TouchEnd(func(event *gr.Event) {
+		// 	k.noteEvent(keyNumber, false)
+		// }),
+	)
+	return key
+}
+
+func isBlackKey(n int) bool {
+	return n == 1 || n == 3 || n == 6 || n == 8 || n == 10
+}
+
+func (k *Keyboard) renderOctave(parent *vdom.Element, keyStart int, xStart int) {
+	for noteType := 0; noteType < 2; noteType++ {
+		xPos := xStart
+		for keyNumber := 0; keyNumber < 12; keyNumber++ {
+			if keyNumber > 0 && !isBlackKey(keyNumber) {
+				xPos += 40
+			}
+			if noteType == 0 && !isBlackKey(keyNumber) {
+				key := k.renderKey(keyNumber+keyStart, false, xPos, k.keydown[keyNumber+keyStart])
+				parent.AppendChild(key)
+			}
+			if noteType == 1 && isBlackKey(keyNumber) {
+				key := k.renderKey(keyNumber+keyStart, true, xPos, k.keydown[keyNumber+keyStart])
+				parent.AppendChild(key)
+			}
+		}
+	}
+}
+
+// Render displays the keyboard.
+func (k *Keyboard) Render() vdom.Element {
+	elem := vdom.MakeElement("g")
+	for octave := 0; octave < 3; octave++ {
+		k.renderOctave(&elem, 48+octave*12, (40*7*octave)+1)
+	}
+
+	return elem
+}
+
+// // ComponentDidMount registers DOM event handler for physical keyboard actions
+// func (k *KeyboardFrontend) ComponentDidMount() {
+// 	var keyMap = map[string]int{
+// 		"a": 60, "w": 61, "s": 62, "e": 63, "d": 64,
+// 		"f": 65, "t": 66, "g": 67, "y": 68, "h": 69, "u": 70, "j": 71, "k": 72,
+// 	}
+//
+// 	doc := js.Global.Get("document")
+// 	doc.Call("addEventListener", "keydown", func(event *js.Object) {
+// 		midiKey := keyMap[event.Get("key").Call("toLowerCase").String()]
+// 		if midiKey != 0 {
+// 			k.noteEvent(midiKey, true)
+// 		}
+// 	})
+// 	doc.Call("addEventListener", "keyup", func(event *js.Object) {
+// 		midiKey := keyMap[event.Get("key").Call("toLowerCase").String()]
+// 		if midiKey != 0 {
+// 			k.noteEvent(midiKey, false)
+// 		}
+// 	})
+// }
