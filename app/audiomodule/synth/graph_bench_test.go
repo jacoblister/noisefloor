abba33894e97092c70e62751a216d43a642e2660
Jacob Lister <jacob.lister@nzregen.co.nz>
2019-05-10 12:01:50 +1200

    add more benchmarks

--- app/audiomodule/synth/graph_bench_test.go
+++ app/audiomodule/synth/graph_bench_test.go
@@ -1,6 +1,7 @@
 package synth
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/jacoblister/noisefloor/app/audiomodule/synth/processor"
@@ -11,6 +12,41 @@ func BenchmarkSingleCall(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		gain.Process(1, 1)
+	}
+}
+
+func BenchmarkSingleCallWithArray(b *testing.B) {
+	gain := processor.Gain{}
+	params := []float32{0, 0}
+
+	callGain := func(gain *processor.Gain, in []float32) []float32 {
+		in[0] = gain.Process(in[0], in[1])
+		return in
+	}
+
+	for i := 0; i < b.N; i++ {
+		params[0] = 1
+		params[1] = 1
+		callGain(&gain, params)
+	}
+}
+
+func BenchmarkSingleCallWithMethodValues(b *testing.B) {
+	gain := processor.Gain{}
+	callGain := gain.Process
+
+	for i := 0; i < b.N; i++ {
+		callGain(1, 1)
+	}
+}
+
+func BenchmarkSingleCallWithReflection(b *testing.B) {
+	gain := processor.Gain{}
+	processMethod := reflect.ValueOf(&gain).MethodByName("Process")
+	values := []reflect.Value{reflect.ValueOf(float32(1.0)), reflect.ValueOf(float32(1.0))}
+
+	for i := 0; i < b.N; i++ {
+		processMethod.Call(values)
 	}
 }
 
