f1288fe9ecf5e4340fed13713e234d3d5dd208c9
Jacob Lister <jacob.lister@nzregen.co.nz>
2019-05-14 16:30:46 +1200

    synthUI - add processor connector point method

--- app/audiomodule/synth/synthUI/processor.go
+++ app/audiomodule/synth/synthUI/processor.go
@@ -7,11 +7,13 @@ import (
 	"github.com/jacoblister/noisefloor/pkg/vdom"
 )
 
+const procWidth = 40
+const procHeight = 80
+const procConnWidth = 6
+
 // Processor is a synth processor block (with inputs and outputs displayed)
 type Processor struct {
 	ProcessorDefinition *synth.ProcessorDefinition
-
-	editState editState
 }
 
 //MakeProcessor create an new Processor UI componenet
@@ -20,23 +22,33 @@ func MakeProcessor(processorDefinition *synth.ProcessorDefinition) *Processor {
 	return &processor
 }
 
+//GetConnectorPoint gets to co-ordinates of an input or output connection point
+func (p *Processor) GetConnectorPoint(isInput bool, index int) (x int, y int) {
+	if isInput {
+		x = p.ProcessorDefinition.X + (procConnWidth / 2)
+		y = p.ProcessorDefinition.Y + (index+1)*(procConnWidth*2) + (procConnWidth / 2)
+		return x, y
+	}
+
+	x = p.ProcessorDefinition.X + (procConnWidth / 2)
+	y = p.ProcessorDefinition.Y + (index+1)*(procConnWidth*2) + (procConnWidth / 2)
+	return x, y
+}
+
 // Render displays a processor
 func (p *Processor) Render() vdom.Element {
-	procWidth := 40
-	procHeight := 80
-
 	procName, procInputs, procOutputs := p.ProcessorDefinition.Processor.Definition()
 
 	inConnectors := []vdom.Element{}
 	for i := 0; i < len(procInputs); i++ {
 		connector := vdom.MakeElement("rect",
 			"id", procName+":inconn:"+strconv.Itoa(i),
-			"x", p.ProcessorDefinition.X+0,
-			"y", p.ProcessorDefinition.Y+(i+1)*8,
-			"width", 4,
-			"height", 4,
+			"x", p.ProcessorDefinition.X,
+			"y", p.ProcessorDefinition.Y+(i+1)*(procConnWidth*2),
+			"width", procConnWidth,
+			"height", procConnWidth,
 			"stroke", "black",
-			"fill", "black",
+			"fill", "white",
 			"cursor", "crosshair",
 		)
 		inConnectors = append(inConnectors, connector)
@@ -46,12 +58,12 @@ func (p *Processor) Render() vdom.Element {
 	for i := 0; i < len(procOutputs); i++ {
 		connector := vdom.MakeElement("rect",
 			"id", procName+":inconn:"+strconv.Itoa(i),
-			"x", p.ProcessorDefinition.X+procWidth-4,
-			"y", p.ProcessorDefinition.Y+(i+1)*8,
-			"width", 4,
-			"height", 4,
+			"x", p.ProcessorDefinition.X+procWidth-procConnWidth,
+			"y", p.ProcessorDefinition.Y+(i+1)*(procConnWidth*2),
+			"width", procConnWidth,
+			"height", procConnWidth,
 			"stroke", "black",
-			"fill", "black",
+			"fill", "white",
 			"cursor", "crosshair",
 		)
 		outConnectors = append(outConnectors, connector)
@@ -66,6 +78,7 @@ func (p *Processor) Render() vdom.Element {
 			"height", procHeight,
 			"stroke", "black",
 			"fill", "white",
+			"cursor", "pointer",
 		),
 		inConnectors,
 		outConnectors,
