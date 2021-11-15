c3f277ed75380fcb18c154699b97c8d0417569f8
Jacob Lister <jacob.h.lister@gmail.com>
2021-11-15 23:44:57 +1300

    add int/float benchmark

--- build/v8engine/Processor/processor_bench_test.go
+++ build/v8engine/Processor/processor_bench_test.go
@@ -2,6 +2,28 @@ package cppprocessor
 
 import "testing"
 
+func BenchmarkAddInt(b *testing.B) {
+	var x int32 = 1
+	var y int32 = 1
+	var res int32
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 1000; j++ {
+			res += x + y
+		}
+	}
+}
+
+func BenchmarkAddFloat32(b *testing.B) {
+	var x float32 = 1.2
+	var y float32 = 1.5
+	var res float32
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 1000; j++ {
+			res += x + y
+		}
+	}
+}
+
 func BenchmarkNothing(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 	}
