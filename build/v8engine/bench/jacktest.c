d2465c8f1acc38a436854d7e61a1bfe06db3e706
Jacob Lister <jacob.h.lister@gmail.com>
2021-11-19 00:15:38 +1300

    benchmark basic patch

--- build/v8engine/bench/jacktest.c
+++ build/v8engine/bench/jacktest.c
@@ -10,12 +10,14 @@ jack_port_t *midi_input_port;
 jack_client_t *client;
 
 float synth_freq = 0;
+float synth_trigger = 0;
 float synth_gate = 0;
 
 int jack_process(jack_nframes_t nframes, void *args) {
     void *midi_in = jack_port_get_buffer(midi_input_port, 0);
     int midi_event_count = jack_midi_get_event_count(midi_in);
     
+    synth_trigger = 0;
     for(int i = 0; i < midi_event_count; i++) {
         jack_midi_event_t read_event;
         jack_midi_event_get(&read_event, midi_in, i);
@@ -23,17 +25,22 @@ int jack_process(jack_nframes_t nframes, void *args) {
     
         int midi_type = ebuffer[0] >> 4;
         int midi_note = ebuffer[1];
-        if (midi_type == 8 || midi_type == 9) {
+        if (midi_type == 9) {
             synth_freq = 220 * pow(2.0, (midi_note - 57) / 12.0);
-            synth_gate = midi_type == 9 ? 1 : 0;
+            synth_trigger = 1;
+            synth_gate = 1;
+            printf("note on freq=%f\n", synth_freq);
+        } 
 
-            printf("synth freq=%f, gate=%f\n", synth_freq, synth_gate);
+        if (midi_type == 8) {
+            synth_gate = 0;
+            printf("note off\n");
         }
     }
     
     jack_default_audio_sample_t *out = jack_port_get_buffer(audio_output_port, nframes);
 
-    synth_process(nframes, out, synth_freq, synth_gate);
+    synth_process(nframes, out, synth_freq, synth_trigger, synth_gate);
     return 0;
 }
 
--- build/v8engine/bench/processor.h
+++ build/v8engine/bench/processor.h
@@ -1,5 +1,7 @@
 #include "math.h"
 
+/* Oscillator */
+
 struct osc {
     float waveTable[48000];
     float currentSample;
@@ -14,29 +16,119 @@ static inline void osc_start(struct osc *osc, int sampleRate) {
     }
 }
 
-static inline float osc_process(struct osc *osc, float freq) {
+static inline float osc_process(struct osc *osc, float frq) {
     float result = osc->waveTable[(int)osc->currentSample];
 
-    osc->currentSample += freq;
+    osc->currentSample += frq;
     if (osc->currentSample >= osc->sampleRate) { osc->currentSample -= osc->sampleRate; }
 
     return result;
 }
 
+/* Envelope */
+
+struct env {
+    float Attack;
+    float Decay;
+    float Sustain;
+    float Release;
+
+    float sampleRate;
+    float output;
+    int step;
+    float delta;
+    float lastTrigger;
+};
+
+static inline void env_start(struct env *env, int sampleRate) {
+    env->sampleRate = sampleRate;
+}
+
+static inline float env_process(struct env *env, float trg, float gte) {
+    if (trg > 0 && env->lastTrigger == 0) {
+        env->output = 0;
+        env->delta = (1000 / env->Attack) / env->sampleRate;
+        env->step = 1;
+    }
+
+    env->output += env->delta;
+
+    switch (env->step) {
+    case 1:
+        if (env->output > 1) {
+            env->delta = (1000 / -env->Decay) / env->sampleRate;
+            env->step = 2;
+        }
+        break;
+    case 2:
+        if (env->output < env->Sustain) {
+            env->delta = 0;
+            env->step = 3;
+        }
+        break;
+    case 3:
+        if (gte == 0) {
+            env->delta = (1000 / -env->Release) / env->sampleRate;
+            env->step = 4;
+        }
+        break;
+    case 4:
+        if (env->output < 0) {
+            env->output = 0;
+            env->delta = 0;
+            env->step = 0;
+        }
+        break;
+    }
+
+    env->lastTrigger = 0;
+    return env->output;
+}
+
+/* Gain */
+
+struct gai {
+};
+
+static inline void gai_start(struct gai *gai, int sampleRate) {
+}
+
+static inline float gai_process(struct gai *gai, float in, float gain) {
+    return in * gain;
+}
+
+
+/* Patch */
+
 struct patch {
     struct osc osc;
+    struct env env;
+    struct gai gai;
 };
 
 static struct patch patch;
 
 static inline void synth_start() {
     osc_start(&patch.osc, 48000);
+
+    patch.env.Attack = 2;
+    patch.env.Decay = 100;
+    patch.env.Sustain = 0.75;
+    patch.env.Release = 1000;
+    env_start(&patch.env, 48000);
+
+    gai_start(&patch.gai, 48000);
 }
 
-static inline void synth_process(int length, float *samples, float freq, float gate) {
+static inline void synth_process(int length, float *samples, float freq, float trigger, float gate) {
     for (int i = 0; i < length; i++) {
-        samples[i] = osc_process(&patch.osc, freq) * gate;
+        float osc = osc_process(&patch.osc, freq);
+        float env = env_process(&patch.env, trigger, gate);
+        float out = gai_process(&patch.gai, osc, env);
+        
+        samples[i] = out;
     }
+
 }
 
 // #define FRAME_SIZE 1024
